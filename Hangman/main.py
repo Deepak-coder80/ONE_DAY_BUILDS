39ad0c1bdf211f5c8c69e0c683e759f3eb409493
Deepak M S <souparnikad40@gmail.com>
2022-06-20 18:17:04 +0530

    basic code added --HangMan

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,14 +1,17 @@
 import random
 
+#create sample wordlist
 word_list = ["aardvark", "baboon", "camel"]
 
-
+#choose a word from the wordlist
 chosen_word = random.choice(word_list)
 
+#read a character from the user
 guess = input("Enter a character : ").lower()
 
+#look for the matched letter
 for letter in chosen_word:
-        if guess == letter:
-                    print("Right")
-                        else:
-                                    print("Wrong")
+    if guess == letter:
+        print("Right")
+    else:
+            print("Wrong")
